27cb9913ba9b6cf16c0965449426ae224530e342
avelez <alberto.velez@solid-stack.com>
2021-11-06 18:39:56 +0100

    Refactoring

---
+++ src/assets/js/projects.js
@@ -0,0 +1,13 @@
+const projects = [
+	{
+		id: 101,
+		title: 'Portfolio',
+		image: 'images/project-images/alt-dough.JPG',
+		technologies: ['HTML & CSS', 'Vue.js', 'SCSS'],
+		description: 'Portfolio creado con Vue.js 2',
+		github_link: 'https://github.com/avelezdo/portfolio-vue',
+		production_link: 'https://www.alternativedough.com',
+	},
+];
+
+export { projects };
