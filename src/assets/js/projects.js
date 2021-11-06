d4f27d7ad2ccbbe07d9c3c6090be93c47e92dfad
avelez <alberto.velez@solid-stack.com>
2021-11-06 20:50:34 +0100

    Se añade imagen para el proyecto de portfolio

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -2,11 +2,11 @@ const projects = [
 	{
 		id: 101,
 		title: 'Portfolio',
-		image: 'images/project-images/alt-dough.JPG',
+		image: 'images/project-images/portfolio.jpg',
 		technologies: ['HTML & CSS', 'Vue.js', 'SCSS'],
 		description: 'Portfolio creado con Vue.js 2',
 		github_link: 'https://github.com/avelezdo/portfolio-vue',
-		production_link: 'https://www.alternativedough.com',
+		production_link: 'https://www.albertovelez.dev',
 	},
 ];
 
