ae48b34c71df4621efbc80f2a0ace3ac1bb0035c
avelezdo <albertovelezdev@gmail.com>
2021-11-15 17:54:04 +0100

    Se añade el proyecto 'Perfiles'

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -1,12 +1,21 @@
 const projects = [
 	{
-		id: 101,
+		id: 1,
 		title: 'Portfolio',
 		image: 'images/project-images/portfolio.jpg',
 		technologies: ['HTML & CSS', 'Vue.js', 'SCSS'],
 		description: 'Portfolio creado con Vue.js 2',
 		github_link: 'https://github.com/avelezdo/portfolio-vue',
 		production_link: 'https://www.albertovelez.dev',
+	},
+	{
+		id: 2,
+		title: 'Perfiles',
+		image: 'images/project-images/profiles.jpg',
+		technologies: ['HTML & CSS', 'Vue.js', 'SCSS', 'Node.js/Express', 'Vuetify'],
+		description: 'Lista, guarda y elimina perfiles de la API RandomAPI',
+		github_link: 'https://github.com/avelezdo/profiles',
+		production_link: '',
 	},
 ];
 
