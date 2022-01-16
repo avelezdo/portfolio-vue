a5b12e3dc8e4dd4e90055eb4d4f6fc03fa063daa
avelezdo <albertovelezdev@gmail.com>
2022-01-16 08:32:47 +0100

    Refactoring

--- src/assets/js/render-projects.js
+++ src/assets/js/render-projects.js
@@ -5,31 +5,22 @@ let skills = ['Proyectos recientes', 'SCSS', 'Vue.js 2', 'Vue.js 3', 'Node.js/Ex
 let selectedSkill = null;
 
 let renderSkills = function (num) {
-	// map through the array of skills and return a
-	// new array with the HTML to render each skill button
 	let ul = document.querySelector('.list__skills'),
 		newArr = skills.map((skill, index) => {
-			if (index == Number.parseInt(num)) {
+			if (index === Number.parseInt(num)) {
 				selectedSkill = skill;
 				return `<li class="skill-selector skill__active" data-id="${index}">${skill}</li>`;
 			} else {
 				return `<li class="skill-selector skill__inactive" data-id="${index}">${skill}</li>`;
 			}
 		});
-	// attach the above generated HTML to the page
-	// then, add the event listener to each button
 	ul.innerHTML = newArr.join('');
-	// document.querySelectorAll('project').forEach(project => {
-	//   project.classList.add('slide-out-to-right');
-	// })
 	attachSkillEvent();
 };
 
 function attachSkillEvent() {
 	let skillSelectors = document.querySelectorAll('.skill-selector');
 	const projectsDestination = document.querySelector('.projects');
-	// cycle through the skill buttons and make it so that
-	// they render the associated project for that skill
 	skillSelectors.forEach((skill) => {
 		skill.addEventListener('mousedown', function (e) {
 			renderSkills(e.target.dataset.id);
@@ -46,49 +37,43 @@ function attachSkillEvent() {
 let renderProjects = function (arr, onload = false) {
 	const projectList = document.querySelector('.list__projects'),
 		allProjects = document.querySelectorAll('.project');
-	// loader = document.querySelector('.projects #loader');
-	// adding the exit animation
 	allProjects.forEach((project, index) => {
-		// project.classList.remove(`project-enter-${index + 1}`);
 		if (!onload) {
 			project.classList.add(`project-exit-${index + 1}`);
 		}
 	});
-	// render the HTML necessary for each
-	// project in the given array
+
 	setTimeout(function () {
 		projectList.innerHTML = arr
 			.map((project, index) => {
 				return `
-      <li class="project project-enter-${index + 1}">
-        <div class="project__description">
-          <div class="project__header-row">
-            <p class="section__headline project__name">${project.title}</p>
-            <div>
-              ${addProductionLink(project)}
-              ${addGitHubLink(project)}
+        <li class="project project-enter-${index + 1}">
+            <div class="project__description">
+            <div class="project__header-row">
+                <p class="section__headline project__name">${project.title}</p>
+                <div>
+                    ${addProductionLink(project)}
+                    ${addGitHubLink(project)}
+                </div>
             </div>
-          </div>
-          <p class="project__description section__copy">
-            ${project.description}
-          </p>
-          <ul class="project__tech" data-project-name="recordshare">
-            ${addTechnologies(project)}
-          </ul>
-        </div>
-        <div>
-          <img src="${project.image}" alt="${project.title} UI"
-            data-project-name="${project.title.toLowerCase()}" class="img project__img">
-        </div>
-      </li>
+            <p class="project__description section__copy">
+                ${project.description}
+            </p>
+            <ul class="project__tech" data-project-name="recordshare">
+                ${addTechnologies(project)}
+            </ul>
+            </div>
+            <div>
+            <img src="${project.image}" alt="${project.title} UI"
+                data-project-name="${project.title.toLowerCase()}" class="img project__img">
+            </div>
+        </li>
     `;
 			})
 			.join('');
 	}, 500);
 
-	document.querySelector('.projects-listed').innerHTML = `
-    ${arr.length === 1 ? `${arr.length} de ${myProjects.length} proyecto listado` : `${arr.length} of ${myProjects.length} proyectos listados`}
-  `;
+	document.querySelector('.projects-listed').innerHTML = `${arr.length} de ${myProjects.length} proyectos`;
 };
 
 let addProductionLink = function (project) {
