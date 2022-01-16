361937de43a88468affc99945e1f2a1a12a43ee7
avelezdo <albertovelezdev@gmail.com>
2022-01-16 08:14:09 +0100

    Limpieza

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,6 @@
 			<About />
 			<Work />
 			<Projects />
-			<!-- <Testimonials /> -->
-			<!-- <Contact /> -->
 		</div>
 		<Footer />
 	</div>
@@ -17,9 +15,7 @@ import Navbar from './components/Navbar.vue';
 import About from './components/About.vue';
 import Work from './components/Work.vue';
 import Projects from './components/Project.vue';
-// import Contact from './components/Contact.vue';
 import Footer from './components/Footer.vue';
-// import Testimonials from './components/Testimonials.vue';
 
 import initScroll from './assets/js/scroll.js';
 
@@ -30,24 +26,10 @@ export default {
 		About,
 		Work,
 		Projects,
-		// Testimonials,
-		// Contact,
 		Footer,
 	},
 	mounted: async function () {
-		// animations
 		initScroll();
 	},
 };
 </script>
-
-<style lang="scss" scoped>
-// #app {
-// 	font-family: Avenir, Helvetica, Arial, sans-serif;
-// 	-webkit-font-smoothing: antialiased;
-// 	-moz-osx-font-smoothing: grayscale;
-// 	text-align: center;
-// 	color: #2c3e50;
-// 	margin-top: 60px;
-// }
-</style>
